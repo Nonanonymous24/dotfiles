fac139a1a28524894ad19fe1a6ab458c80679400
Nonanonymous24 <sawantaashay24@gmail.com>
2023-04-09 02:04:08 +0530

    zsh script update

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -11,6 +11,11 @@ config_zsh() {
 
 	# powerlevel10k
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	p10k configure
+
+	# plugins
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 	# create directory structure
 	if [ -d "~/.zsh" ]; then
