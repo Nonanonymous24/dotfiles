662a454ea0421bb1981559127d52ba1736cab7ad
Nonanonymous24 <sawantaashay24@gmail.com>
2023-04-14 08:47:33 +0530

    added nvm config to nvim.sh

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -5,6 +5,15 @@ config_nvim() {
 	sudo pacman -S --needed lazygit ripgrep fd cmake cargo xclip python-pynvim ruby git curl
 	yay -S --needed neovim-nightly
 
+	# install node and npm using nvm
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+	source ~/.nvm/nvm.sh
+	source ~/.bashrc
+
+	nvm install node
+	nvm install-latest-npm
+
 	# create directory structure
 	if [ -d ~/.config/nvim ]; then
 		rm -rfv ~/.config/nvim
